7f731fae0f4d2f9459ce64b56dc89f80a2283729
GenanawMekete <104999951+GenanawMekete@users.noreply.github.com>
2026-01-22 16:23:45 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+export const BOT_TOKEN = process.env.BOT_TOKEN;
+export const WEBAPP_URL = "https://YOUR-NETLIFY-APP.netlify.app";
+export const BACKEND_URL = "https://YOUR-RENDER-BACKEND.onrender.com";
